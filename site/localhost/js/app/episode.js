e974d1090ca26e993c96a314c6e05ea399a50135
docluv <chris@love2dev.com>
2019-11-04 04:46:08 -0500

    doing some general cleanup

--- site/localhost/js/app/episode.js
+++ site/localhost/js/app/episode.js
@@ -1,41 +1,68 @@
-(function () {
+( function () {
 
-    var listenLater = document.querySelector(".btn-listen-later");
+    var listenLater = document.querySelector( ".btn-listen-later" );
 
-    if (listenLater) {
+    if ( listenLater ) {
 
-        listenLater.addEventListener("click", function (evt) {
+        listenLater.addEventListener( "click", function ( evt ) {
 
-            var guid = listenLater.getAttribute("data-id");
+            var guid = listenLater.getAttribute( "data-id" );
 
-            if (guid) {
-
+            if ( guid ) {
+                saveEpisodeData( guid );
             }
 
-        });
+        } );
 
     }
 
-    function saveEpisodeData(guid) {
+    function saveEpisodeData( guid ) {
 
         var episodeSource = "api/episodes/" + guid + ".json";
 
-        fetch(episodeSource)
-            .then(function (response) {
+        fetch( episodeSource )
+            .then( function ( response ) {
 
-                if (response && response.ok) {
+                if ( response && response.ok ) {
 
-                    caches.open("LISTEN_LATER").then(
+                    var resp = response.clone();
+
+                    caches.open( "LISTEN_LATER" ).then(
                         cache => {
 
-                            cache.put(episodeSource, response);
+                            cache.put( episodeSource, response );
 
-                        });
+                        } );
+
+                    resp.json()
+                        .then( function ( episode ) {
+
+                            saveEpisodeAudio( episode.src );
+
+                        } );
 
                 }
 
-            });
+            } );
 
     }
 
-})();
+    function saveEpisodeAudio( src ) {
+
+        fetch( src, {
+                mode: "no-cors"
+            } )
+            .then( function ( response ) {
+
+                if ( response && [ 0, 200 ].includes( response.status ) ) {
+
+                    localforage.setItem( src, response.blob() );
+
+                }
+
+            } );
+
+    }
+
+
+} )();
